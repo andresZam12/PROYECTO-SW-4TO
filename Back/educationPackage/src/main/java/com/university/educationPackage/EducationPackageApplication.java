969ee608a06ce275d1612d5de4fbaf4322b22465
Andrés Zambrano <giovanny.zambranob@campusucc.edu.co>
2025-05-07 00:09:59 -0500

    inicialización Springboot

---
+++ Back/educationPackage/src/main/java/com/university/educationPackage/EducationPackageApplication.java
@@ -0,0 +1,13 @@
+package com.university.educationPackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationPackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationPackageApplication.class, args);
+	}
+
+}
---
+++ Back/educationPackage/src/test/java/com/university/educationPackage/EducationPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.educationPackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationPackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
